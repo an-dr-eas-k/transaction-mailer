dddf18a75af1afaa0b385a3f3c70543ef12b11de
andreask <andreas.kirschner@gmail.com>
2016-01-07 20:32:40 +0100

    update tests and build

--- src/test/java/net/andreask/banking/integration/db/AccountTransactionFacadeTest.java
+++ src/test/java/net/andreask/banking/integration/db/AccountTransactionFacadeTest.java
@@ -1,21 +1,49 @@
 package net.andreask.banking.integration.db;
 
 import net.andreask.banking.integration.db.model.AccountTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
- *
  * @author andreask
  */
 
-@RunWith(MockitoJUnitRunner.class)
-public class AccountTransactionFacadeTest{
+@RunWith(Arquillian.class)
+public class AccountTransactionFacadeTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        JavaArchive ja = ShrinkWrap.create(JavaArchive.class)
+                .addPackage(AccountTransactionFacade.class.getPackage())
+                .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        System.out.println(ja.toString(true));
+        return ja;
+    }
 
-    @InjectMocks
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void should_create_greeting() {
+        Assert.fail("Not yet implemented");
+    }
+
+    @Inject
     AccountTransactionFacade atf;
+
     @Test
     public void testCreate() throws Exception {
 
