0de9eed26befb50b9079804b402e9b898573263e
AndreasK <andreas.kirschner@gmail.com>
2016-07-09 23:33:48 +0200

    fix migration v1

--- src/main/server/db/sql/migration-v1.sql
+++ src/main/server/db/sql/migration-v1.sql
@@ -1,2 +1,4 @@
+connect 'jdbc:derby:/var/glassfish/databases/hv';
 alter table ACCOUNTCONNECTION drop column hbciversion;
 alter table ACCOUNTCONNECTION add TITLE varchar (255);
+describe ACCOUNTCONNECTION;
