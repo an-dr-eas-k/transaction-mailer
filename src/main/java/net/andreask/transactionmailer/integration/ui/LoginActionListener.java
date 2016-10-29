4424c75294ef0bb4fd54feb021b3f8388e7a2235
AndreasK <andreas.kirschner@gmail.com>
2016-10-29 23:02:17 +0200

    introduce userlogin

--- src/main/java/net/andreask/transactionmailer/integration/ui/LoginActionListener.java
+++ src/main/java/net/andreask/transactionmailer/integration/ui/LoginActionListener.java
@@ -1,12 +1,25 @@
 package net.andreask.transactionmailer.integration.ui;
 
+import javax.el.ELResolver;
+import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ActionListener;
 
 public class LoginActionListener implements ActionListener {
-	public void processAction(ActionEvent e) 
-	  throws AbortProcessingException {
-	  System.out.println("logging in ...........");
-	}
+
+  public void processAction(ActionEvent e)
+      throws AbortProcessingException {
+
+    FacesContext fc = FacesContext.getCurrentInstance();
+    ELResolver elResolver = fc.getApplication().getELResolver();
+
+    User user = (User) elResolver.getValue(
+        fc.getELContext(), null, "user");
+
+    if (!user.getName().equals("andreask")) {
+      throw new IllegalAccessError("wrong user");
+    }
+    System.out.println("logging in ...........");
+  }
 }
