7da4b13b8dd450f1419aec9c571fa56bde72d230
an-dr-eas-k <16319480+an-dr-eas-k@users.noreply.github.com>
2023-04-02 20:24:26 +0200

    hide user data

--- src/main/server/javamail/create-java-mail-resource.sh
+++ src/main/server/javamail/create-java-mail-resource.sh
@@ -3,13 +3,15 @@ jndi_name="mail/hv"
 
 # dir=$(cygpath . -wa)
 
+read -p "mailhost>" mailhost
+read -p "fromaddress>" fromaddress
+read -p "mailuser>" mailuser
 read -p "passwd>" passwd
 
 #echo -n > pw-file
 
 #printf "AS_ADMIN_PASSWORD=%s\n" $passwd >> pw-file
 #printf "AS_ADMIN_USERPASSWORD=%s\n" $passwd >> pw-file
-#printf "AS_ADMIN_nch812p1PASSWORD=%s\n" $passwd >> pw-file
 
 cd "${glassfish_home}"/bin
 
@@ -18,16 +20,16 @@ cd "${glassfish_home}"/bin
 #./asadmin \
 #create-javamail-resource \
 #--mailhost smtp.gmail.com \
-#--mailuser andreas.kirschner@gmail.com \
-#--fromaddress andreas.kirchner@gmail.com \
+#--mailuser name@gmail.com \
+#--fromaddress name@gmail.com \
 #--property mail.smtp.socketFactory.port=465:mail.smtp.port=465:mail.smtp.socketFactory.fallback=false:mail.smtp.auth=true:mail.smtp.password=${passwd}:mail.smtp.socketFactory.class=javax.net.ssl.SSLSocketFactory \
 #${jndi_name}
 
 ./asadmin \
 create-javamail-resource \
---mailhost smtp.netclusive.de \
---mailuser nch812p1 \
---fromaddress "tm <transaction-mailer@zimmer3.de>" \
+--mailhost ${mailhost} \
+--mailuser ${mailuser} \
+--fromaddress "tm <${fromaddress}>" \
 --property mail.smtp.auth=true:mail.smtp.password=${passwd} \
 ${jndi_name}
 
